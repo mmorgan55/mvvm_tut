09454a0e9e16a464447e6b034352c8032d5b240d
Matthew Morgan <mmorgan55@cnm.edu>
2019-03-01 21:00:03 -0700

    Initial commit

---
+++ app/src/main/java/mmorgan/tutorial/android/myapplication/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package mmorgan.tutorial.android.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
