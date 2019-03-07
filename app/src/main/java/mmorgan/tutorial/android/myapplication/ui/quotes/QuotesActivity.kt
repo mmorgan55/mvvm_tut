2001a6a946fa365779f54ccf130f11ce34daa0a5
Matthew Morgan <mmorgan55@cnm.edu>
2019-03-07 12:54:03 -0700

    Added UI functionality to be able to type out quotes and add them to database.

--- app/src/main/java/mmorgan/tutorial/android/myapplication/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/mmorgan/tutorial/android/myapplication/ui/quotes/QuotesActivity.kt
@@ -2,12 +2,40 @@ package mmorgan.tutorial.android.myapplication.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_quotes.*
 import mmorgan.tutorial.android.myapplication.R
+import mmorgan.tutorial.android.myapplication.data.Quote
+import mmorgan.tutorial.android.myapplication.utilities.InjectorUtils
+import java.lang.StringBuilder
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        val factory = InjectorUtils.providesQuotesViewModelFactory()
+        val viewModel = ViewModelProviders.of(this, factory)
+            .get(QuotesViewModel::class.java)
+
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach{quote ->
+                stringBuilder.append("$quote\n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuote(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
