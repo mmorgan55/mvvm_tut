d616b89349fc1fd4863ceb4075240dd66d415bdd
Matthew Morgan <mmorgan55@cnm.edu>
2019-03-07 11:53:17 -0700

    Migrated project to AndroidX.

--- app/src/main/java/mmorgan/tutorial/android/myapplication/QuotesActivity.kt
+++ app/src/main/java/mmorgan/tutorial/android/myapplication/QuotesActivity.kt
@@ -1,6 +1,6 @@
 package mmorgan.tutorial.android.myapplication
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class QuotesActivity : AppCompatActivity() {
